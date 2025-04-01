3a57e1c0c12bedce183a2a2c24c29dce0420c24a
Chris Milne <milne_c1@sky.com>
2025-04-01 13:23:05 +0100

    Add files via upload

---
+++ Concrete-Bags/Concrete-Bags/Concrete_BagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concrete_BagsApp.swift
+//  Concrete-Bags
+//
+//  Created by Chris Milne on 01/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Concrete_BagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainMenu()
+        }
+    }
+}
